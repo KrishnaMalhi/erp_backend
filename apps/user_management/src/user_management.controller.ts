d0821ab92ef13ac19ac938c9684ee463448cc096
KrishnaMahli <kirshna.malhi066.com>
2023-04-16 23:04:54 +0500

    user management service basic crud operations

--- apps/user_management/src/user_management.controller.ts
+++ apps/user_management/src/user_management.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { UserManagementService } from './user_management.service';
+import { MessagePattern } from '@nestjs/microservices';
 
+interface NotifiyData {
+  user: string;
+  data: object;
+}
 @Controller()
 export class UserManagementController {
-  constructor(private readonly userManagementService: UserManagementService) {}
+  constructor(private readonly userManagementService: UserManagementService) { }
 
-  @Get()
-  getHello(): string {
-    return this.userManagementService.getHello();
+  @MessagePattern('notify')
+  async notify(data: NotifiyData) {
+    console.log('send')
+    Logger.log("notificatoin data" + data.user);
+    let a: number = data.data['a'];
+    let b: number = data.data['b'];
+    console.log(a, b)
+    return a + b;
   }
+
+
 }
